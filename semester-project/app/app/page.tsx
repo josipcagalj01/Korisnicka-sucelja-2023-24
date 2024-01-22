90ef20621e2a045d7afa5574f2131748f529bbed
josipcagalj01 <jcagal00@fesb.hr>
2024-01-22 21:44:54 +0100

    Određene izmjene izgleda pocetne stranice, poboljšana responzivnost

--- semester-project/app/app/page.tsx
+++ semester-project/app/app/page.tsx
@@ -7,49 +7,56 @@ import Link from 'next/link'
 import { Suspense } from 'react';
 import Loading from './components/Loading/loading'
 import Image from 'next/image'
-import digitalsociety from 'public/digitalsociety.jpg'
+import bilazgrada from 'public/bilazgrada.jpg'
+import cloudservices from 'public/cloudservices.png'
+import BorderedLink from './components/BorderedLink/button'
 
 export const metadata: Metadata = {
-  title: 'Početna | eKaštela',
-  description: 'Početna stranica informacijskog sustava eKaštela',
+	title: 'Početna | eKaštela',
+	description: 'Početna stranica informacijskog sustava eKaštela',
 }
 
 export default function Home() {
-  return (
-    <>
-      
-      <div className="flex justify-center landingBanner">
-        <div className='textContainerContainer'>
-            <div className='textContainer'>
-              <h1 className='HomePageLargeTitle'>e-Kaštela</h1>
-              <p className='HomePageDescriptions'>Usluge lokalne samouprave za XXI. stoljeće</p>
-              <Link href='/prijava' className='borderedLink'><p className='linkText'>Započnite s korištenjem</p></Link>
-          </div>
-        </div>
-      </div>
-      <main className='homePageMain'>
-        
-        <div className="HomePageAnnouncmentsBanner">
-          <h2>Najnovije obavijesti</h2>
-          <Suspense fallback={<Loading message='Učitavanje obavijesti'/>}>
-            <News limit={4} offset={0} desiredId={undefined}/>
-          </Suspense>
-          <Link href='/obavijesti?_page=1&_limit=10' className='marginLeftAuto displayBlock fitContent borderedLink'> <p className='linkText'>Sve obavijesti</p> </Link>
-        </div>
-        <div className="flex justify-center HomePageBanner Reverse">
-          <div className='HomePageBannerTextContainer'>
-            <div className='textContainer'>
-              <h1 className='HomePageLargeTitle'>Bilo kad. Bilo gdje. S bilo kojeg uređaja.</h1>
-              <p className='HomePageDescriptions'>Otkrijte novu dimenziju komunikacije s gradskom upravom, ustanovama i tvrtkama.</p>
-              <Link href='/o-sustavu' className='borderedLink'> <p className='linkText'>Pregled dostupnih usluga</p> </Link>
-            </div>
-          </div><Image src={digitalsociety.src} height={500} width={500} alt='slika3'/>
-          
-        </div>
-      </main>
-      
-    </>
-  );
+	return (
+		<>
+			<div className="flex justify-center landingBanner">
+				<div className='textContainerContainer'>
+					<div className='textContainer'>
+						<h1 className='HomePageLargeTitle'>e-Kaštela</h1>
+						<p className='HomePageDescriptions'>Usluge lokalne samouprave za XXI. stoljeće</p>
+						<BorderedLink href='/prijava'>Započnite s korištenjem</BorderedLink>
+					</div>
+				</div>
+			</div>
+			<main className='homePageMain'>
+				<div className="HomePageAnnouncmentsBanner">
+					<h2>Najnovije obavijesti</h2>
+					<Suspense fallback={<Loading message='Učitavanje obavijesti' />}>
+						<News limit={4} offset={0} desiredId={undefined} />
+					</Suspense>
+					<BorderedLink href='/obavijesti?_page=1&_limit=10' className='marginLeftAuto displayBlock'>Sve obavijesti</BorderedLink>
+				</div>
+				<div className='HomePageBanner'>
+					<h2>Opće informacije</h2>
+					<div className='generalInfoContainer'>
+						<div className='imageAndLinkContainer'>
+							<div className='imageContainer'>
+							<Image src={bilazgrada.src} fill={true} sizes='300px' quality={100}  style={{ objectFit: 'cover' }} alt='uvjetikoristenja' />
+							</div>
+							<Link href='/o-sustavu'> <h3>Uvjeti korištenja</h3> </Link>
+						</div>
+						<div className='imageAndLinkContainer'>
+							<div className='imageContainer'>
+								<Image src={cloudservices.src} fill={true} sizes='300px' style={{ objectFit: 'cover' }} alt='uvjetikoristenja' />
+							</div>
+							<Link href='#'> <h3>Katalog usluga</h3> </Link>
+						</div>
+					</div>
+				</div>
+
+			</main>
+		</>
+	);
 }
 
 /*body className={inter.className}*/
