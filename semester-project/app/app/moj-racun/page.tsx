8dcd1cbb44182e1deaaf7fbc06cc7807ff3d720a
josipcagalj01 <jcagal00@fesb.hr>
2024-07-24 19:22:28 +0200

    Maknuta nespremna komponenta

--- semester-project/app/app/moj-racun/page.tsx
+++ semester-project/app/app/moj-racun/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import AccountSettingsWrapper from '../components/accountSettingsWrapper';
 import AccountSettingsMenu from '../components/accountSettingsMenu';
-import EmployeeActions from '../components/employeeActions';
+
 import './accountSettingsMenuStyle.css'
 
 export const metadata: Metadata = {
@@ -13,7 +13,7 @@ async function ManageMyAccount() {
 	return (
     <main className='accountPageMain'>
 			<AccountSettingsWrapper>
-        <EmployeeActions/>
+
         <AccountSettingsMenu/>
       </AccountSettingsWrapper>
     </main>
