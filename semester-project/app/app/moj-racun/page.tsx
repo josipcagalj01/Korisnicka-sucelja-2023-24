8ab1d27668082d5020421ed15e7fffbe58a33685
josipcagalj01 <jcagal00@fesb.hr>
2024-07-24 19:15:41 +0200

    Maknuta nepotrebna klasa

--- semester-project/app/app/moj-racun/page.tsx
+++ semester-project/app/app/moj-racun/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import AccountSettingsWrapper from '../components/accountSettingsWrapper';
 import AccountSettingsMenu from '../components/accountSettingsMenu';
+import EmployeeActions from '../components/employeeActions';
 import './accountSettingsMenuStyle.css'
 
 export const metadata: Metadata = {
@@ -10,11 +11,12 @@ export const metadata: Metadata = {
 
 async function ManageMyAccount() {
 	return (
-    <main className='accountPageMain formMain'>
+    <main className='accountPageMain'>
 			<AccountSettingsWrapper>
+        <EmployeeActions/>
         <AccountSettingsMenu/>
       </AccountSettingsWrapper>
     </main>
-    )
+  )
 }
 export default ManageMyAccount;
