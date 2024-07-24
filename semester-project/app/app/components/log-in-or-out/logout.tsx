008a261c001335fbf78421946f7c6777b37068e3
josipcagalj01 <jcagal00@fesb.hr>
2024-07-24 18:17:31 +0200

    Dodana propagacija imena klase za komponentu za odjavu

--- semester-project/app/app/components/log-in-or-out/logout.tsx
+++ semester-project/app/app/components/log-in-or-out/logout.tsx
@@ -5,14 +5,14 @@ import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-function LogInOrOut({callbackUrl}:{callbackUrl?:string}) {
+function LogInOrOut({callbackUrl, className}:{callbackUrl?:string, className?:string}) {
 	let url = '/prijava'
 	if(callbackUrl) url = url + `?callbackUrl=${callbackUrl}`
 	const session = useSession()
 	switch(session.status) {
-		case('loading') : return <Image src='/spinner.gif' height={15} width={15} alt='spinner' style={{margin:'auto'}}/>
-		case('authenticated') : return <button onClick={() => signOut({callbackUrl:url})} type='submit'>Odjava</button>
-		case('unauthenticated') : return <Link href='/prijava'>Prijava</Link>
+		case('loading') : return <Image className={className} src='/spinner.gif' height={15} width={15} alt='spinner' style={{margin:'auto'}}/>
+		case('authenticated') : return <button className={className} onClick={() => signOut({callbackUrl:url})} type='submit'>Odjava</button>
+		case('unauthenticated') : return <Link className={className} href='/prijava'>Prijava</Link>
 	}
 }
 export default LogInOrOut
