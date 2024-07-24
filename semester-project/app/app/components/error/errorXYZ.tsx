d82b89b217b02eef55c399085ea7a30d7c8d885f
josipcagalj01 <jcagal00@fesb.hr>
2024-07-24 18:07:28 +0200

    Dodana komponenta za gresku 403

--- semester-project/app/app/components/error/errorXYZ.tsx
+++ semester-project/app/app/components/error/errorXYZ.tsx
@@ -1,14 +1,33 @@
 import BorderedLink from "../BorderedLink/button";
 import '../../not-foundStyle.css'
+import { Session } from "../../../lib/getSession";
+import LogInOrOut from "../log-in-or-out/logout";
+import { usePathname } from "next/navigation";
 
-export function Error401({callbackUrl}:{callbackUrl:string | undefined}) {
+export function Error401({callbackUrl, message, session}:{callbackUrl?:string, message?:string, session?:Session}) {
+	const errorMessage = message || 'Vaša sesija je istekla ili je prekinuta. Da biste vidjeli sadržaj stranice ponovo se prijavite.'
 	return (
 		<div className='responsiveContainer'>
-			<div className='notFoundMessage'>
+			<div className='errorInfo'>
 				<h1 className='errorCode'>401</h1>
 				<h1>Greška</h1>
-				<p>Vaša sesija je istekla ili prekinuta. Da biste vidjeli sadržaj stranice ponovo se prijavite.</p>
-				<BorderedLink href={`/prijava?callbackUrl=${callbackUrl}`} className='A'>Prijava</BorderedLink>
+				<p>{errorMessage}</p>
+				<BorderedLink href={`/prijava${callbackUrl ? '?callbackUrl='+callbackUrl: '' }`} className='A'>Prijava</BorderedLink>
+			</div>
+		</div>
+	)
+}
+
+export function Error403() {
+	const path = usePathname()
+	return (
+		<div className='responsiveContainer'>
+			<div className='errorInfo'>
+				<h1 className='errorCode'>403</h1>
+				<h1>Greška</h1>
+				<p>Pristup ovoj stranici omogućen je samo autoriziranim korisnicima. Da biste vidjeli njen sadržaj, prijavite se korisničkim računom povezanim s primjerenom razinom prava.</p>
+				<LogInOrOut className="borderedButton" callbackUrl={path}/>
+				<BorderedLink href='/' className='A'>Povratak na početnu stranicu</BorderedLink>
 			</div>
 		</div>
 	)
@@ -17,7 +36,7 @@ export function Error401({callbackUrl}:{callbackUrl:string | undefined}) {
 export function Error404() {
 	return (
 		<div className='responsiveContainer'>
-			<div className='notFoundMessage'>
+			<div className='errorInfo'>
 				<h1 className='errorCode'>404</h1>
 				<h1>Greška</h1>
 				<p>Stranicu koju ste namjeravali posjetiti nije moguće pronaći. Možda je uklonjena ili premještena.</p>
