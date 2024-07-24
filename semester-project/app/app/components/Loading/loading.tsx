3e9fbb5433861c786b2a67a20d8c47894c50ec9e
josipcagalj01 <jcagal00@fesb.hr>
2024-07-24 13:59:37 +0200

    Sitno refaktoriranje koda komponente za prikaz učitavanja

--- semester-project/app/app/components/Loading/loading.tsx
+++ semester-project/app/app/components/Loading/loading.tsx
@@ -10,12 +10,12 @@ type loadingProps = {
 	width?:number
 }
 
-function Loading({message, bold=false, color='', height=100, width=100}:loadingProps) {
-	const style = {'color': color ?? '', 'fontWeight': bold ? 'bold' : 'normal'}
+function Loading({message, bold, color, height=100, width=100}:loadingProps) {
+	const style = {'color': color || '', 'fontWeight': bold ? 'bold' : 'normal'}
     return (
 		<div className='loadingSpinnerContainer' style={{margin:'auto'}}>
 			<Image src={spinner.src} alt='ucitavanje' width={width} height={height} style={{margin:'auto'}}/>
-			<p  style={style} className='loadingMessage'>{message ?? 'a'}</p>
+			<p style={style} className='loadingMessage'>{message}</p>
 		</div>
 	)
 }
