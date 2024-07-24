ab0152bd39462764e8c524ba4aaa4a1b3b66648d
josipcagalj01 <jcagal00@fesb.hr>
2024-07-24 14:28:54 +0200

    Dodavanje datoteke zaboravljene pri prethodnom commitu

--- semester-project/app/app/components/accountSettingsMenu.tsx
+++ semester-project/app/app/components/accountSettingsMenu.tsx
@@ -4,18 +4,18 @@ import BorderedLink from './BorderedLink/button'
 import { actions } from "../moj-racun/[action]/page"
 
 export default function AccountSettingsMenu() {
-    return (
-        <div className='accountPageContent'>
-			<h1>Postavke korisničkog računa</h1>
-            <ul className='accountSettingsMenu'>
-                {actions.map((action=>(
-                    <li key={action.parameter}>
-                        <Image src={action.actionThumbnail} height={200} width={200} alt={action.text}/>
-                        <BorderedLink href={'/moj-racun/'+action.parameter}>{action.text}</BorderedLink>
-                    </li>
-                )))}
-                        
-            </ul>
-        </div>
-    )
+	return (
+		<div className='accountPageContent'>
+			<h2>Postavke korisničkog računa</h2>
+			<ul className='accountSettingsMenu'>
+				{actions.map((action => (
+					<li key={action.formName}>
+						<Image src={action.actionThumbnail} height={200} width={200} alt={action.text} />
+						<BorderedLink href={'/moj-racun/' + action.text.toLowerCase().replaceAll(' ', '-').replaceAll('č', 'c').replaceAll('š', 's')}>{action.text}</BorderedLink>
+					</li>
+				)))}
+
+			</ul>
+		</div>
+	)
 }
