e6b4f3a11092b6268e3846b36c8ab0aa110b9fbe
josipcagalj01 <jcagal00@fesb.hr>
2024-07-24 18:09:06 +0200

    Dodana komponenta za gumb karakteristicnog izgleda

--- semester-project/app/app/components/BorderedLink/button.tsx
+++ semester-project/app/app/components/BorderedLink/button.tsx
@@ -4,13 +4,17 @@ import Link from "next/link"
 import './borderedLinkStyle.css'
 
 interface linkProps {
-    href:Url,
-    children:React.ReactNode,
-    className?: string
+	href: Url,
+	children: React.ReactNode,
+	className?: string
 }
 
-export default function BorderedLink({href, children, className=''}:linkProps) {
-    return (
-        <Link href={href ?? ''} className={`borderedLink ${className}`}><p className='linkText'>{children}</p></Link>
-    )
+export default function BorderedLink({ href, children, className = '' }: linkProps) {
+	return (
+		<Link href={href ?? ''} className={`borderedLink ${className}`}><p className='linkText'>{children}</p></Link>
+	)
+}
+
+export function BorderedButton({ onClick, children }: { onClick: any, children: React.ReactNode }) {
+	return (<button className='borderedLink' onClick={onClick}><p className='linkText'>{children}</p></button>)
 }
