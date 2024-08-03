5e4ca9048d8d510a4750a189cec6e18397b34364
josipcagalj01 <jcagal00@fesb.hr>
2024-08-03 23:33:15 +0200

    Navigacija po stranicama izdvojena u zasebnu komponentu

--- semester-project/app/app/obavijesti/page.tsx
+++ semester-project/app/app/obavijesti/page.tsx
@@ -8,13 +8,14 @@ import { getTotalNewsCount } from '../../lib/contentfulClient'
 import { Suspense } from 'react';
 import Loading from '../components/Loading/loading'
 import BorderedLink from '../components/BorderedLink/button'
+import PagesNavigation from '../components/PagesNavigation'
 
 export const metadata: Metadata = {
 	title: 'Obavijesti',
 	description: 'Stranica s obavijestima informacijskog sustava eKaštela',
 }
 
-var newsCategories = ['Sve obavijesti', 'Natječaji', 'Usluge', 'Servisne informacije']
+var newsCategories = ['Natječaji', 'Usluge', 'Servisne informacije']
 
 async function Announcments({ searchParams }: { searchParams: Record<string, string | string[] | undefined>; }) {
 
@@ -24,36 +25,23 @@ async function Announcments({ searchParams }: { searchParams: Record<string, str
 	const totalAnnouncments = await getTotalNewsCount({ category })
 	const totalPages = Math.ceil(totalAnnouncments / pageSize);
 
-	function PagesNavigation() {
-		return (
-			<div className="flex gap-4 pagesNavigation">
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: 1, _limit: pageSize, _category: _category?.toString() } }} className={`${page < 2 && 'disabled'} desktop`}>Prva</BorderedLink>
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: 1, _limit: pageSize, _category: _category?.toString() } }} className={`${page < 2 && 'disabled'} mobile`}>{`«`}</BorderedLink>
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: page - 1, _limit: pageSize, _category: _category?.toString() } }} className={`${page < 2 && 'disabled'} desktop`}>Prethodna</BorderedLink>
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: page - 1, _limit: pageSize, _category: _category?.toString() } }} className={`${page < 2 && 'disabled'} mobile`}>{`‹`}</BorderedLink>
-				<span className='currentPageInfo'> Stranica <br className='mobile' />{page} od {totalPages} </span>
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: page + 1, _limit: pageSize, _category: _category?.toString() } }} className={`${(totalPages - page) < 1 && 'disabled'} desktop`}>Sljedeća</BorderedLink>
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: page + 1, _limit: pageSize, _category: _category?.toString() } }} className={`${(totalPages - page) < 1 && 'disabled'} mobile`}>{`›`}</BorderedLink>
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: totalPages, _limit: pageSize, _category: _category?.toString() } }} className={`${(totalPages - page) < 1 && 'disabled'} desktop`}>Posljednja</BorderedLink>
-				<BorderedLink href={{ pathname: `/obavijesti`, query: { _page: totalPages, _limit: pageSize, _category: _category?.toString() } }} className={`${(totalPages - page) < 1 && 'disabled'} mobile`}>{`»`}</BorderedLink>
-			</div>
-		)
-	}
+	const query = {'_limit': pageSize, '_category': category}
 
 	if (page > totalPages) return (<main className='errorMain'><Error404 /></main>)
 	return (
 		<main>
 			<h1>Obavijesti</h1>
 			<div className='categoryMenuContainer'>
+				<BorderedLink key='Sve obavijesti' className={!category ? 'current' : ''} href='/obavijesti?_page=1&_limit=10'>Sve obavijesti</BorderedLink>
 				{newsCategories.map((Category) => (
-					<BorderedLink key={Category} className={Category === category?.replace('_', ' ') ? 'current' : Category === 'Sve obavijesti' && !category ? 'current' : ''} href={`/obavijesti?_page=1&_limit=10${Category === 'Sve obavijesti' ? '' : `&_category=${Category.replace(' ', '_')}`}`}>{Category}</BorderedLink>
+					<BorderedLink key={Category} className={Category === category?.replace('_', ' ') ? 'current' : ''} href={`/obavijesti?_page=1&_limit=10${Category === 'Sve obavijesti' ? '' : `&_category=${Category.replace(' ', '_')}`}`}>{Category}</BorderedLink>
 				))}
 			</div>
-			{_limit && _page && <PagesNavigation />}
+			{_limit && _page && <PagesNavigation basePath='obavijesti' page={page} totalPages={totalPages} otherParams={query}/>}
 			<Suspense fallback={<Loading message='Učitavanje obavijesti ...' />}>
 				<News offset={pageSize * (page - 1)} limit={pageSize} category={_category?.toString()} className='mobile-no-thumbnail' />
 			</Suspense>
-			{_limit && _page && <PagesNavigation />}
+			{_limit && _page && <PagesNavigation basePath='obavijesti' page={page} totalPages={totalPages} otherParams={query}/>}
 		</main>
 	);
 }
