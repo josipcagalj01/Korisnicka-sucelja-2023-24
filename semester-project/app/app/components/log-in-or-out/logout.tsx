1260266ee41f3e56be097496ce2677b027f35a2a
josipcagalj01 <jcagal00@fesb.hr>
2024-09-19 23:34:37 +0200

    Gif zamijenjen html-css vrtilicom

--- semester-project/app/app/components/log-in-or-out/logout.tsx
+++ semester-project/app/app/components/log-in-or-out/logout.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link';
-import Image from 'next/image';
+import Loader from '../loader/loader';
 
 function LogInOrOut({callbackUrl, className, spinnerSize=15}:{callbackUrl?:string, className?:string, spinnerSize?:number}) {
 	let url = '/prijava'
 	if(callbackUrl) url = url + `?callbackUrl=${callbackUrl}`
 	const session = useSession()
 	switch(session.status) {
-		case('loading') : return <figure style={{margin:'auto', position:'relative', width: `${spinnerSize}px`, aspectRatio: 1}}><Image src='/spinner.gif' fill={true} alt='spinner' style={{objectFit: 'contain'}}/></figure>
+		case('loading') : return <Loader size={spinnerSize}/>
 		case('authenticated') : return <button className={className} onClick={() => signOut({callbackUrl:url})} type='button'>Odjava</button>
 		case('unauthenticated') : return <Link className={className} href='/prijava'>Prijava</Link>
 	}
