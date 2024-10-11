b9c9d8bb95afc8631cbe12d0e9c373bba0cbc7aa
josipcagalj01 <jcagal00@fesb.hr>
2024-10-11 22:49:02 +0200

    Komponenti za obavijest o uspjesnosti radnje se od sad moze pridijeliti i klasa po zelji

--- semester-project/app/app/components/actionResultInfo/actionResultInfo.tsx
+++ semester-project/app/app/components/actionResultInfo/actionResultInfo.tsx
@@ -2,14 +2,15 @@ import './errorInfoStyle.css'
 import Image from 'next/image'
 interface errorInfoProps {
     ok?:boolean
-    message: string
+    message: string,
+    className?: string
 }
 
-export default function ActionResultInfo({ok=false, message}: errorInfoProps) {
+export default function ActionResultInfo({ok=false, message, className}: errorInfoProps) {
     let imageSrc='/erroricon.png'
     if(ok) imageSrc='/ok-icon.png'
     return (
-        <div className="actionResultInfoInfoContainer">
+        <div className={"actionResultInfoInfoContainer " + className}>
             <Image className='actionResultInfoImage' src={imageSrc} width={80} height={80} alt='greska'/>
             <p className='actionResultInfoMessage'>{message}</p>
         </div>
