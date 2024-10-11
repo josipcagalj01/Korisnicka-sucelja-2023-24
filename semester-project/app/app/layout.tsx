ab01506be39234dff4c49c9748ff74aa70186155
josipcagalj01 <jcagal00@fesb.hr>
2024-10-11 03:15:44 +0200

    Stavljen prozirni favicon

--- semester-project/app/app/layout.tsx
+++ semester-project/app/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<html lang="hr">
-			<head><link rel='icon' href={favicon.src} /></head>
+			
 			<body>
 				<AuthProvider>
 					<Header />
