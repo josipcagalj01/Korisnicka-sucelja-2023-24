2542e106eed11ea305986d08947f7596df32d9cb
josipcagalj01 <jcagal00@fesb.hr>
2024-10-12 17:45:16 +0200

    Dodane stranice pristup kojima treba stititi

--- semester-project/app/middleware.ts
+++ semester-project/app/middleware.ts
@@ -1,2 +1,11 @@
 export { default } from "next-auth/middleware"
-export const config = { matcher: ["/moj-racun", '/moj-racun/:action*'] }
+export const config = { 
+	matcher: [
+		"/moj-racun", '/moj-racun/:action*',
+		'/usluge', '/usluge/:serviceId*', 
+		"/api/dodaj-obrazac", "/aši/update-form",
+		"/obrasci", "/obrasci/:option*",
+		"/prijave", "/prijave/:serviceId*", "/prijave/:serviceId*/:submissionId*",
+		"/api/upload-form-thumbnail", "/api/upload-form-thumbnail/:id"
+	] 
+}
