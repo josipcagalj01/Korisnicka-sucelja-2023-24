5a18fffb884c6cb74a13bea2883f0eee8073d2b1
josipcagalj01 <jcagal00@fesb.hr>
2024-10-12 17:46:12 +0200

    Ispravljeno krivo slovo

--- semester-project/app/middleware.ts
+++ semester-project/app/middleware.ts
@@ -3,7 +3,7 @@ export const config = {
 	matcher: [
 		"/moj-racun", '/moj-racun/:action*',
 		'/usluge', '/usluge/:serviceId*', 
-		"/api/dodaj-obrazac", "/aši/update-form",
+		"/api/dodaj-obrazac", "/api/update-form",
 		"/obrasci", "/obrasci/:option*",
 		"/prijave", "/prijave/:serviceId*", "/prijave/:serviceId*/:submissionId*",
 		"/api/upload-form-thumbnail", "/api/upload-form-thumbnail/:id"
