c05a58d0a05a977354e3a377f2993483859c0350
josipcagalj01 <jcagal00@fesb.hr>
2024-10-12 23:16:55 +0200

    Ispravljena nelogicnost kod dohvacanja naslovne slike obrasca

--- semester-project/app/app/api/datoteka/obrasci/[...options]/route.ts
+++ semester-project/app/app/api/datoteka/obrasci/[...options]/route.ts
@@ -4,7 +4,7 @@ import { MultipleParams } from "../../../../../lib/interfaces";
 import jwt from "jsonwebtoken";
 
 export async function GET(req: Request, {params}: {params: MultipleParams}) {
-	if(params.options.length===2) {
+	
 		try {
 			if(!isNaN(parseInt(params.options[0]))) {
 				const id = parseInt(params.options[0])
@@ -42,6 +42,6 @@ export async function GET(req: Request, {params}: {params: MultipleParams}) {
 			console.error(error)
 			return NextResponse.json({ message: "Dogodila se greška! Nije moguće dohvatiti datoteku." }, { status: 500 })
 		}
-	}
-	else return NextResponse.json({ message: "Nisu navedeni svi potrebni parametri." }, { status: 400 })
+	
+	
 }
