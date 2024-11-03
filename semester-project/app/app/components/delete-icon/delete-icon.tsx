79cf2c822c5655ac40605fb636db0781af19f4ae
josipcagalj01 <jcagal00@fesb.hr>
2024-11-03 23:41:47 +0100

    Izmjena komponente ikone za uklanjanje necega

--- semester-project/app/app/components/delete-icon/delete-icon.tsx
+++ semester-project/app/app/components/delete-icon/delete-icon.tsx
@@ -1,11 +1,13 @@
 'use client'
 import styles from './deleteIconStyle.module.css'
 
-export default function DeleteIcon({size='15px', className='', onClick}:{size?: string, className?: string, onClick?:React.MouseEventHandler<HTMLButtonElement>}) {
+export default function DeleteIcon({size=15, thickness=3, className='', onClick}:{size?: number, thickness?:number, className?: string, onClick?:React.MouseEventHandler<HTMLDivElement>}) {
+	const first_translate_Y = Math.ceil(size/2) - Math.ceil(thickness/2)
+	const second_translate_Y = first_translate_Y - thickness
 	return (
-		<button type='button' id={styles.deleteicon} className={className} onClick={onClick} style={{height: size}}>
-			<div id={styles.first} style={{width: 'calc(size * 1.4142 - 3px)'}}></div>
-			<div id={styles.second} style={{width: 'calc(size * 1.4142 - 3px)'}}></div>
-		</button>
+		<div  id={styles.deleteicon}  className={className} onClick={onClick} style={{height: `${size}px`, width: `${size}px`}}>
+			<div id={styles.first} style={{height: thickness, transform: `translateY(${first_translate_Y}px) rotate(-45deg)`}}></div>
+			<div id={styles.second} style={{height: thickness, transform: `translateY(${second_translate_Y}px) rotate(45deg)`}}></div>
+		</div>
 	)
 }
