a15ce4507cab6a6f2044e2c7a5811ae29dd0b10f
josipcagalj01 <jcagal00@fesb.hr>
2024-11-03 23:18:44 +0100

    Refaktotriranje komponente PagesNavigation

--- semester-project/app/app/components/pages-navigation/PagesNavigation.tsx
+++ semester-project/app/app/components/pages-navigation/PagesNavigation.tsx
@@ -11,31 +11,32 @@ export interface pagesNavigationProps {
 	otherParams?: urlQuery}
 
 export default function PagesNavigation({className='', basePath, page, totalPages, otherParams} : pagesNavigationProps) {
+	const {_page, ...rest} = otherParams || {_page: undefined}
 	return (
 		<div className={"flex gap-4 " + styles.pagesNavigation + ' ' + className}>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: 1, ...otherParams} }} className={`${page < 2 && styles.disabled} ${styles.desktop}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: 1, ...rest} }} className={`${page < 2 && styles.disabled} ${styles.desktop}`}>
 				Prva
 			</BorderedLink>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: 1, ...otherParams} }} className={`${page < 2 && styles.disabled} ${styles.mobile} ${styles.back}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: 1, ...rest} }} className={`${page < 2 && styles.disabled} ${styles.mobile} ${styles.back}`}>
 				<Image alt='show-hide-arrow-double.png' src='/arrows/show-hide-arrow-double.png' height={13} width={13}/>
 			</BorderedLink>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page-1, ...otherParams} }} className={`${page < 2 && styles.disabled} ${styles.desktop}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page-1, ...rest} }} className={`${page < 2 && styles.disabled} ${styles.desktop}`}>
 				Prethodna
 			</BorderedLink>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page-1, ...otherParams} }} className={`${page < 2 && styles.disabled} ${styles.mobile}  ${styles.back}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page-1, ...rest} }} className={`${page < 2 && styles.disabled} ${styles.mobile}  ${styles.back}`}>
 				<Image alt='show-hide-arrow.png' src='/arrows/show-hide-arrow.png' height={14} width={8}/>
 			</BorderedLink>
 			<span className='currentPageInfo'> Stranica <br className='mobile'/>{page} od {totalPages} </span>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page+1, ...otherParams} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.desktop}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page+1, ...rest} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.desktop}`}>
 				Sljedeća
 			</BorderedLink>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page+1, ...otherParams} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.mobile}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: page+1, ...rest} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.mobile}`}>
 				<Image alt='show-hide-arrow.png' src='/arrows/show-hide-arrow.png' height={14} width={8}/>
 			</BorderedLink>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: totalPages, ...otherParams} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.desktop}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: totalPages, ...rest} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.desktop}`}>
 				Posljednja
 			</BorderedLink>
-			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: totalPages, ...otherParams} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.mobile}`}>
+			<BorderedLink href={{ pathname: `/${basePath}`, query: { _page: totalPages, ...rest} }} className={`${(totalPages - page) < 1 && styles.disabled} ${styles.mobile}`}>
 				<Image alt='show-hide-arrow-double.png' src='/arrows/show-hide-arrow-double.png' height={13} width={13}/>
 			</BorderedLink>
 		</div>
