3b8fd61b476c709474885e28e2c7d768af2a8db6
josipcagalj01 <jcagal00@fesb.hr>
2024-11-07 12:44:38 +0100

    Lijepse oblikovan kod, maknute nepotrebne stvari

--- semester-project/app/app/api/upload-form-thumbnail/[id]/route.ts
+++ semester-project/app/app/api/upload-form-thumbnail/[id]/route.ts
@@ -10,70 +10,54 @@ interface Params {
 export async function POST(request: Request, {params}: {params: Params}): Promise<NextResponse> {
 	const body = (await request.json()) as HandleUploadBody;
 	try {
-		
-		
-		
-		/*else {
-			const form = await db.form.findUnique({where: {id: id}})
-			if(!form) return NextResponse.json({message: 'Ne postoji obrazac čija je oznaka navedena.'}, {status: 404})
-			else if(user?.department_id !== form.department_id && (user?.role_id!==1 && user?.role_id!==3)) return NextResponse.json({message: 'Nemate dopuštenje izvesti ovu radnju.'}, {status: 403})
-			else {*/
-		console.log('tu smo')
-				const jsonResponse = await handleUpload({
-					body,
-					request,
-					onBeforeGenerateToken: async (
-						pathname,
-						/* clientPayload */
-					) => {
-						const id = parseInt(params.id)
-						const {user} = await getSession() || {user:null}
-						if(isNaN(id)) throw new Error('Invalid form id format provided')/*return NextResponse.json({message: 'Unesen je krivi oblik oznake obrasca'}, {status: 400})*/
-						else {
-							const form = await db.form.findUnique({where: {id: id}})
-							if(!form) throw new Error('Form whose id is ... does not exist') /*NextResponse.json({message: 'Ne postoji obrazac čija je oznaka navedena.'}, {status: 404})*/
-							else if(user?.department_id !== form.department_id && (user?.role_id!==1 && user?.role_id!==3)) throw new Error('Access denied')/*return NextResponse.json({message: 'Nemate dopuštenje izvesti ovu radnju.'}, {status: 403})*/
-						}
-						// Generate a client token for the browser to upload the file
-						// ⚠️ Authenticate and authorize users before generating the token.
-						// Otherwise, you're allowing anonymous uploads.
-		
-						return {
-							allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
-							tokenPayload: JSON.stringify({
-								// optional, sent to your server on upload completion
-								// you could pass a user id from auth, or a value from clientPayload
-							}),
-						};
-					},
-					onUploadCompleted: async ({ blob, tokenPayload }) => {
-						// Get notified of client upload completion
-						// ⚠️ This will not work on `localhost` websites,
-						// Use ngrok or similar to get the full upload flow
-		
-						console.log('blob upload completed', blob, tokenPayload);
-						const id = parseInt(params.id)
-						try {
-							// Run any logic after the file upload completed
-							// const { userId } = JSON.parse(tokenPayload);
-							// await db.update({ avatar: blob.url, userId });
-							const new_thumbnail = await db.form_thumbnail.create({data: {name: blob.url.split('/').slice(-1)[0]}})
-							await db.form.update({
-								data: {
-									thumbnail: {
-										connect: {id: new_thumbnail.id}
-									},
-									thumbnail_setting: 'existing'
-								},
-								where: {id: id}})
-						} catch (error) {
-							throw new Error('Could not update form');
-						}
-					},
-				});
-				return NextResponse.json(jsonResponse);
-			
-		
+		const jsonResponse = await handleUpload({
+			body,
+			request,
+			onBeforeGenerateToken: async (
+				pathname,
+				/* clientPayload */
+			) => {
+				const id = parseInt(params.id)
+				const { user } = await getSession() || { user: null }
+				if (isNaN(id)) throw new Error('Invalid form id format provided')
+				else {
+					const form = await db.form.findUnique({ where: { id: id } })
+					if (!form) throw new Error('Form whose id is ... does not exist')
+					else if (user?.department_id !== form.department_id && (user?.role_id !== 1 && user?.role_id !== 3)) throw new Error('Access denied')
+				}
+
+				return {
+					allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
+					tokenPayload: JSON.stringify({
+						// optional, sent to your server on upload completion
+						// you could pass a user id from auth, or a value from clientPayload
+					}),
+				};
+			},
+			onUploadCompleted: async ({ blob, tokenPayload }) => {
+				// Get notified of client upload completion
+				// ⚠️ This will not work on `localhost` websites,
+				// Use ngrok or similar to get the full upload flow
+
+				console.log('blob upload completed', blob, tokenPayload);
+				const id = parseInt(params.id)
+				try {
+					const new_thumbnail = await db.form_thumbnail.create({ data: { name: blob.url.split('/').slice(-1)[0] } })
+					await db.form.update({
+						data: {
+							thumbnail: {
+								connect: { id: new_thumbnail.id }
+							},
+							thumbnail_setting: 'existing'
+						},
+						where: { id: id }
+					})
+				} catch (error) {
+					throw new Error('Could not update form');
+				}
+			},
+		});
+		return NextResponse.json(jsonResponse);
 	} catch (error) {
 		return NextResponse.json(
 			{ error: (error as Error).message },
