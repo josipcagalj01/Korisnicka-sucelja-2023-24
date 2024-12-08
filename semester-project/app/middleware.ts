facbed0378a29053a0c9e713d1f03aa9282c91cd
josipcagalj01 <jcagal00@fesb.hr>
2024-12-08 12:49:58 +0100

    Dodana zastita pristupa novododanim stranicama

--- semester-project/app/middleware.ts
+++ semester-project/app/middleware.ts
@@ -3,12 +3,16 @@ export const config = {
 	matcher: [
 		"/moj-racun", '/moj-racun/:action*',
 		'/usluge', '/usluge/:serviceId*', 
-		"/api/dodaj-obrazac", "/api/update-form",
-		"/obrasci", "/obrasci/:option*",
+		"/api/dodaj-obrazac", "/api/update-form", "/api/ukloni-obrazac",
+		"/obrasci", "/obrasci/:options*",
 		"/prijave", "/prijave/:serviceId*", "/prijave/:serviceId*/:submissionId*",
 		"/api/upload-form-thumbnail", "/api/upload-form-thumbnail/:id",
 		"/upravljanje-sustavom/upravljanje-korisnicima", "/upravljanje-sustavom/upravljanje-korisnicima/:option*",
 		"/api/update-user",
-		"/api/upload-form-attachment"
+		"/api/upload-form-file",
+		"/api/datoteka/prijave/:option*",
+		"/api/dodaj-objavu", "/api/uredi-objavu/", "/api/ukloni-objavu",
+		"/objave", "/objave/:options*",
+		"/api/upload-announcment-thumbnail", "/api/upload-announcment-file"
 	] 
 }
